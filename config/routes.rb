cc73d4138270a23f1f675413c729036263e81a28
Sid <samanduyev@gmail.com>
2019-06-11 13:12:53 -0400

    changed root

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'operations/index'
   get 'wallets/index'
   post '/auth/login', to: 'authentication#login'
+  root to: 'wallets#index'
   resources :users, only: [:new, :create, :index, :show]
   resources :wallets
   resources :operations
